7cb175f716e8510cc7ec83971d3f78c205bd8115
Yann <yboisvenue@gmail.com>
2019-02-18 12:35:58 -0500

    rajout du back end logend et submit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,85 @@
 // server for final project
+const express = require("express");
+const cors = require("cors");
+const sha256 = require("sha256");
+const fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const bodyParser = require("body-parser");
+const stripe = require("stripe")("sk_test_ARpzQb6KqDa2TWVSTSlh9SQf");
+const url = "mongodb://admin:password1@ds119930.mlab.com:19930/finalproject";
+
+let dbs = undefined;
+MongoClient.connect(url, { useNewUrlParser: true }, (err, allDbs) => {
+  dbs = allDbs;
+});
+//the { useNewUrlParser: true }, bit is only to remove the parse error we continually get
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.raw({ type: "*/*" }));
+
+app.use((req, res, next) => {
+  try {
+    req.body = JSON.parse(req.body.toString());
+    next();
+  } catch (error) {
+    next();
+  }
+});
+
+app.post("/signup", (req, res) => {
+  let db = dbs.db("finalproject");
+  db.collection("users").findOne(
+    { username: req.body.username },
+    (err, result) => {
+      if (err) return res.status(500).send(err);
+      if (result) {
+        res.send(JSON.stringify({ success: false }));
+      } else {
+        let user = {
+          username: req.body.username,
+          password: sha256(req.body.password)
+        };
+        db.collection("users").insertOne(user, (err, result) => {
+          if (err) {
+            if (err) return res.status(500).send(err);
+          }
+        });
+        res.send(JSON.stringify({ success: true }));
+      }
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  let db = dbs.db("finalproject");
+  db.collection("users").findOne(
+    { username: req.body.username },
+    (err, result) => {
+      if (err) return res.status(500).send(err);
+      if (result) {
+        if (result.password === sha256(req.body.password)) {
+          res.send(
+            JSON.stringify({
+              success: true,
+              user: {
+                userId: result._id,
+                username: result.username,
+                location: result.location
+              }
+            })
+          );
+        } else {
+          res.send(JSON.stringify({ success: false }));
+        }
+      } else {
+        res.send(JSON.stringify({ success: false }));
+      }
+    }
+  );
+});
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
+});
