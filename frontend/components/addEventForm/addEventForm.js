cf9016ef6f4391418691485e2b2a56584045e21a
Yann <yboisvenue@gmail.com>
2019-02-25 14:21:59 -0500

    fix of the add event endpoint

--- frontend/components/addEventForm/addEventForm.js
+++ frontend/components/addEventForm/addEventForm.js
@@ -76,7 +76,7 @@ class addEventForm extends Component {
         desc: this.state.desc,
         coordinate: this.state.coordinate
       });
-      fetch("http://localhost:4000/addevent", {
+      fetch("http://68.183.200.44:4000/addevent", {
         method: "POST",
         body: requestBody
       })
